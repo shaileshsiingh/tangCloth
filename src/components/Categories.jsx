cde7210392aa65fe5bddd7dcfb16cbbb681ba53b
shaileshsiingh <shaileshs@yupptv.com>
2025-05-04 17:30:04 +0530

    changes major2

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -273,7 +273,8 @@ function Categories2() {
   const fetchCategories = useCallback(async () => {
     try {
       setLoading(true);
-      const response = await fetch(`http://91.203.135.152:2001/api/category/getAllCategory`);
+      // const response = await fetch(`http://91.203.135.152:2001/api/category/getAllCategory`);
+      const response = await fetch(`${API_URL}/category/getAllCategory`);
 
       if (!response.ok) {
         throw new Error('Failed to fetch categories');
