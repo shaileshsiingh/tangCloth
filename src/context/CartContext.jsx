09e211078f939f44680f9341e3bca580292590fc
shaileshsiingh <shaileshs@yupptv.com>
2025-05-12 01:09:56 +0530

    updated cart ui2

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -212,7 +212,7 @@ export function CartProvider({ children }) {
                             alt={item?.name}
                             className="w-full h-full object-cover rounded-md"
                           />
-                          <div className="absolute -top-2 -right-2 bg-red-500 text-white text-xs font-bold rounded-full w-6 h-6 flex items-center justify-center">
+                          <div className="absolute -top-2 -right-2 bg-cyan-500 text-white text-xs font-bold rounded-full w-6 h-6 flex items-center justify-center">
                             {item.quantity}
                           </div>
                         </div>
